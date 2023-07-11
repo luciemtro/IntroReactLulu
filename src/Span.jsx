1fa87d2677d15927e249fc5277c54fbede25b521
Lucie Monteiro <115341058+luciemtro@users.noreply.github.com>
2023-07-11 16:04:30 +0200

    Add files via upload

---
+++ src/Span.jsx
@@ -0,0 +1,13 @@
+
+function span () {
+    return(
+        <>
+        <span id="top"></span>  
+        <span id="right"></span>  
+        <span id="bottom"></span>
+        <span id="left"></span>
+        </>
+    )
+}
+
+export default span
